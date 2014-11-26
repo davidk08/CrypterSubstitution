351057184871d8d92f577d4eabd017532a98331f
davidkiefer <davidkiefer1994@hotmail.com>
2014-11-26 10:59:04 +0100

    JavaDoc Korrektur

---
+++ TPE Übungsblatt/src/hs_mannheim/tpe/uib_10/pflichtuebung3/AssociativeArray.java
@@ -0,0 +1,25 @@
+package hs_mannheim.tpe.uib_10.pflichtuebung3;
+/** 
+ * Interface fuer die Methoden des typsicheren Assozitiven Arrays.
+ * 
+ * */
+import java.util.function.*;
+
+public interface AssociativeArray<S, T> {
+
+	public void clear();
+	public boolean containsValue(T value);
+	public boolean containsKey(S key);
+	public T get(S key);
+	public boolean isEmpty();
+	public void put(S key, T value);
+	public void putAll(AssociativeArray<S, T> array);
+	public T remove(S key);
+	public int size();
+	public void update(S key, T value);
+	public void forEach(BiConsumer<S, T> consumer);
+	public void extractAll(AssociativeArray<S, T> array);
+	public AssociativeArray<S, T> map(BiFunction<S, T, T > function);
+	
+
+}
